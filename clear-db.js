b8ac69f8ae339c3692d9d7854000d9aabce72122
Varnikumar Patel <varnikumar1512@gmail.com>
2026-07-30 10:56:37 +0530

    remove unused database testing scripts and HLD edge generator utility

--- clear-db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const url = 'mongodb+srv://varni:Varni%401102@kisanhub.gbinuf2.mongodb.net/archmind';
-mongoose.connect(url).then(async () => {
-  const db = mongoose.connection.db;
-  const result = await db.collection('lldhistories').deleteMany({});
-  console.log("Deleted documents:", result.deletedCount);
-  process.exit(0);
-}).catch(console.error);
